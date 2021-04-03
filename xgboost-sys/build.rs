8a91943e284fece347f8b38fffca70325adbcdfd
MashPlant <mashplant@163.com>
2021-04-03 17:42:56 +0800

    disable parallel xgboost

--- xgboost-sys/build.rs
+++ xgboost-sys/build.rs
@@ -1,68 +1,24 @@
-extern crate bindgen;
-
-use std::process::Command;
-use std::env;
-use std::path::{Path, PathBuf};
-
 fn main() {
-    let target = env::var("TARGET").unwrap();
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let xgb_root = Path::new(&out_dir).join("xgboost");
+    let s = std::process::Command::new("make").arg("-j").arg("config=make/minimum.mk")
+        .current_dir("xgboost")
+        .status()
+        .expect("Failed to execute XGBoost make script.");
+    assert!(s.success());
 
-    // copy source code into OUT_DIR for compilation if it doesn't exist
-    if !xgb_root.exists() {
-        Command::new("cp")
-            .args(&["-r", "xgboost", xgb_root.to_str().unwrap()])
-            .status()
-            .unwrap_or_else(|e| {
-                panic!("Failed to copy ./xgboost to {}: {}", xgb_root.display(), e);
-            });
-    }
-
-    // TODO: allow for dynamic/static linking
-    // TODO: check whether rabit should be built/linked
-    if !xgb_root.join("lib").exists() {
-        // TODO: better checks for build completion, currently xgboost's build script can run
-        // `make clean_all` if openmp build fails
-        Command::new(xgb_root.join("build.sh"))
-            .current_dir(&xgb_root)
-            .status()
-            .expect("Failed to execute XGBoost build.sh script.");
-    }
-
-    let xgb_root = xgb_root.canonicalize().unwrap();
-
-    let bindings = bindgen::Builder::default()
+    bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg(format!("-I{}", xgb_root.join("include").display()))
-        .clang_arg(format!("-I{}", xgb_root.join("rabit/include").display()))
+        .clang_arg("-Ixgboost/include")
+        .clang_arg("-Ixgboost/rabit/include")
         .generate()
-        .expect("Unable to generate bindings.");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Unable to generate bindings.")
+        .write_to_file(format!("{}/bindings.rs", std::env::var("OUT_DIR").unwrap()))
         .expect("Couldn't write bindings.");
 
-    println!("cargo:rustc-link-search={}", xgb_root.join("lib").display());
-    println!("cargo:rustc-link-search={}", xgb_root.join("rabit/lib").display());
-    println!("cargo:rustc-link-search={}", xgb_root.join("dmlc-core").display());
-
-    // check if built with multithreading support, otherwise link to dummy lib
-    if xgb_root.join("rabit/lib/librabit.a").exists() {
-        println!("cargo:rustc-link-lib=static=rabit");
-        println!("cargo:rustc-link-lib=dylib=gomp");
-    } else {
-        println!("cargo:rustc-link-lib=static=rabit_empty");
-    }
-
-    // link to appropriate C++ lib
-    if target.contains("apple") {
-        println!("cargo:rustc-link-lib=c++");
-    } else {
-        println!("cargo:rustc-link-lib=stdc++");
-    }
-
+    println!("cargo:rustc-link-search=xgboost/lib");
+    println!("cargo:rustc-link-search=xgboost/rabit/lib");
+    println!("cargo:rustc-link-search=xgboost/dmlc-core");
+    println!("cargo:rustc-link-lib=static=rabit_empty");
+    println!("cargo:rustc-link-lib={}", if std::env::var("TARGET").unwrap().contains("apple") { "c++" } else { "stdc++" });
     println!("cargo:rustc-link-lib=static=dmlc");
     println!("cargo:rustc-link-lib=static=xgboost");
 }
